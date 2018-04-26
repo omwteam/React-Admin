e7899dcdb59d576d85e98c125d780ef472b6de7f
lin-xin <2981207131@qq.com>
2018-04-26 20:37:20 +0800

    完成登录界面

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Route from './router/index.js';
 import registerServiceWorker from './registerServiceWorker';
+import './style/common.css'
 
-ReactDOM.render(<Route />, document.getElementById('root'));
+ReactDOM.render(
+    <Route/>, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,12 +1,80 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import './login.css';
+const FormItem = Form.Item;
 
-
-class Login extends Component{
-    render(){
-        return (
-            <h1>这里是login</h1>
-        )
-    }
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this
+      .props
+      .form
+      .validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+  }
+  render() {
+    const {getFieldDecorator} = this.props.form;
+    return (
+      <div className="login-box">
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <FormItem>
+            {getFieldDecorator('userName', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入用户名'
+                }
+              ]
+            })(
+              <Input
+                prefix={< Icon type = "user" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
+                placeholder="Username"/>
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: '请输入密码'
+                }
+              ]
+            })(
+              <Input
+                prefix={< Icon type = "lock" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
+                type="password"
+                placeholder="Password"/>
+            )}
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator('remember', {
+              valuePropName: 'checked',
+              initialValue: true
+            })(
+              <Checkbox>记住密码</Checkbox>
+            )}
+            <a className="login-form-forgot" href="">忘记密码</a>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    )
+  }
 }
+const WrappedLoginForm = Form.create()(LoginForm);
 
+class Login extends Component {
+  render() {
+    return (
+      <div className="login-wrapper">
+        <WrappedLoginForm/>
+      </div>
+    )
+  }
+}
 export default Login;
