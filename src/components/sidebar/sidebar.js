576507d0f744cdaa5c39b70f9533e73b89cd31d1
lin-xin <2981207131@qq.com>
2018-04-28 14:44:39 +0800

    'sidebar改为动态menu'

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,34 +1,67 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Layout, Menu, Icon} from 'antd';
+import './sidebar.css';
 const {Sider} = Layout;
 
+const menus = [
+    {
+        path: '/dashboard',
+        title: 'Dashboard',
+        icon: 'dashboard'
+    },{
+        path: '/article',
+        title: '文章列表',
+        icon: 'profile'
+    },{
+        path: '/cates',
+        title: '分类管理',
+        icon: 'folder-open'
+    },{
+        path: '/tags',
+        title: '标签管理',
+        icon: 'tags-o'
+    },
+]
+const MenuList = menus.map((item) => {
+    return (
+        <Menu.Item key={item.path}>
+            <Link to={item.path}>
+                <Icon type={item.icon}/>
+                <span>{item.title}</span>
+            </Link>
+        </Menu.Item>
+    )
+})
+
 class Sidebar extends Component {
+    
     render() {
         return (
             <Sider>
-                <div className="logo"/>
+                <div className="logo"><Icon type="global"/> React</div>
                 <Menu theme="dark" mode="inline" defaultSelectedKeys={['/article']}>
-                    <Menu.Item key="/dashboard">
+                    {MenuList}
+                    {/* <Menu.Item key="/dashboard">
                         <Link to="/dashboard">
-                            <Icon type="video-camera"/>
+                            <Icon type="dashboard"/>
                             <span>Dashboard</span>
                         </Link>
                     </Menu.Item>
                     <Menu.Item key="/article">
                         <Link to="/article">
-                            <Icon type="video-camera"/>
+                            <Icon type="profile"/>
                             <span>文章列表</span>
                         </Link>
                     </Menu.Item>
                     <Menu.Item key="2">
-                        <Icon type="video-camera"/>
+                        <Icon type="folder-open"/>
                         <span>分类管理</span>
                     </Menu.Item>
                     <Menu.Item key="3">
-                        <Icon type="upload"/>
+                        <Icon type="tags-o"/>
                         <span>标签管理</span>
-                    </Menu.Item>
+                    </Menu.Item> */}
                 </Menu>
             </Sider>
         )
--- src/pages/article/index.js
+++ src/pages/article/index.js
@@ -1,53 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Table, Icon, Divider, Button} from 'antd';
+import './index.css';
 
-const columns = [{
-    title: '标题',
-    dataIndex: 'title',
-    key: 'title',
-    render: text => <a href="javascript:;">{text}</a>,
-  }, {
-    title: '分类',
-    dataIndex: 'cate',
-    key: 'cate',
-  }, {
-    title: '发布时间',
-    dataIndex: 'time',
-    key: 'time',
-  }, {
-    title: '操作',
-    key: 'action',
-    render: (text, record) => (
-      <span>
-        <Button size="small">编辑</Button>
-        <Button type="danger" size="small">删除</Button>
-      </span>
-    ),
-  }];
-  
-  const data = [{
-    key: '1',
-    title: 'Koa2 之文件上传下载',
-    cate: 'Node.js',
-    time: '2018-03-28 16:43:25',
-  }, {
-    key: '2',
-    title: 'Parcel：常见技术栈的集成方式',
-    cate: '前端工具',
-    time: '2018-03-27 12:41:28',
-  },{
-    key: '3',
-    title: 'HTML5 桌面通知：Notification API',
-    cate: 'HTML5',
-    time: '2018-03-26 09:43:15',
-  }];
+const columns = [
+    {
+        title: '标题',
+        dataIndex: 'title',
+        key: 'title',
+        render: text => <a href="javascript:;">{text}</a>
+    }, {
+        title: '分类',
+        dataIndex: 'cate',
+        key: 'cate'
+    }, {
+        title: '发布时间',
+        dataIndex: 'time',
+        key: 'time'
+    }, {
+        title: '操作',
+        key: 'action',
+        render: (text, record) => (
+            <span>
+                <Button size="small">编辑</Button>
+                <Divider type="vertical"/>
+                <Button type="danger" size="small">删除</Button>
+            </span>
+        )
+    }
+];
 
-class ArticleList extends Component{
-    render(){
+const data = [
+    {
+        key: '1',
+        title: 'Koa2 之文件上传下载',
+        cate: 'Node.js',
+        time: '2018-03-28 16:43:25'
+    }, {
+        key: '2',
+        title: 'Parcel：常见技术栈的集成方式',
+        cate: '前端工具',
+        time: '2018-03-27 12:41:28'
+    }, {
+        key: '3',
+        title: 'HTML5 桌面通知：Notification API',
+        cate: 'HTML5',
+        time: '2018-03-26 09:43:15'
+    }
+];
+
+class ArticleList extends Component {
+    render() {
         return (
             <div>
-                <Button type="primary" icon="plus">新增文章</Button>
-                <Table columns={columns} dataSource={data} />
+                <div className="article-handle">
+                    <Button type="primary" icon="plus">新增文章</Button>
+                </div>
+                <Table columns={columns} dataSource={data}/>
             </div>
         )
     }
